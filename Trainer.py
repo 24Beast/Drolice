bb041b0c6c4ff152c61b6943ed16a3ef5d83d05a
24Beast <40471796+24Beast@users.noreply.github.com>
2019-09-29 00:03:54 +0530

    Add files via upload

--- Trainer.py
+++ Trainer.py
@@ -8,6 +8,7 @@ from frovedis.mllib.linear_model import LogisticRegression # frovedis
 FrovedisServer.initialize("mpirun -np 4 {}".format(os.environ['FROVEDIS_SERVER'])) # frovedis
 X_train=np.load('/usr/uhome/HT0011/X_Train.npy')
 y_train=np.load('/usr/uhome/HT0011/y_train.npy')
+y_train=[:,0]
 y_train = y_train.astype(np.float64)
 y_train = 2 * y_train - 1  # frovedis only supports labels of {-1, 1}
 
