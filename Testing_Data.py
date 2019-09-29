2d32bb0fec01910ff3c9b33faefb959fe3bf4d27
24Beast <40471796+24Beast@users.noreply.github.com>
2019-09-29 10:58:37 +0530

    Add files via upload

--- Testing_Data.py
+++ Testing_Data.py
@@ -4,28 +4,20 @@ import pickle
 import time
 import json
 from frovedis.exrpc.server import FrovedisServer # frovedis
-from frovedis.mllib.svm import LinearSVC
 #from sklearn.linear_model import LogisticRegression # sklearn
 
 FrovedisServer.initialize("mpirun -np 4 {}".format(os.environ['FROVEDIS_SERVER'])) # frovedis
 i=0
-li=[]
-X=np.load('/usr/uhome/HT0011/X_Train.npy')
-y=np.load('/usr/uhome/HT0011/y_train.npy')
-y=y[:,0]
-l=len(X)
-while(i<5):
-    X_train=np.load('/usr/uhome/HT0011/X_Train'+str(i)+'.npy')
-    y_train=np.load('/usr/uhome/HT0011/Drolice/y_train'+str(i)+'.npy')
-#    y_train=y_train[:,0]
-    y_train = 2 * y_train - 1  # frovedis only supports labels of {-1, 1}
+while True:
+    try:
+        X_train=np.load('/usr/uhome/HT0011/X_Train'+str(i)+'.npy')
+    except FileNotFoundError:
+        break
     start=time.time()
-    clf = LinearSVC().fit(X_train, y_train)
+    clf = pickle.load(open("/usr/uhome/HT0011/model/Model1.sav","rb"))
     y_pred = clf.predict(X_train)
-    score = 1.0 * sum(y_train == y_pred) / len(y_train)
     end=time.time()
-    data={"score":score,"Model":"SVC","time":end-start}
+    data={"score":y_pred,"time":end-start}
     json_data=json.dumps(data)
-    li.append(data)
-print(l)
+    
 FrovedisServer.shut_down() # frovedis
